c48e5ffaba0c88768d69266b2169d5877f96cea0
Paul Girardin <paul.girardin@eemi.com>
2016-12-07 00:49:36 +0100

    Modifications to step 4 (but lack of css :/)

--- test/test1.js
+++ test/test1.js
@@ -1,5 +1,6 @@
 function Tekitizy (selector, options) {
   this.selector = selector
+  this.options = options
   if (options && options.hasOwnProperty('carroussel_id')) {
     this.carroussel_id = options.carroussel_id
   } else {
@@ -7,20 +8,15 @@ function Tekitizy (selector, options) {
   }
   this.index = 0
   this.listImg = []
-  this.prevNext = options.prevNext
-  this.play = options.play
   this.auto = options.autoPlay
   this.imageDuration = options.imageDuration
-  this.effect = options.effect
-  // this.selector <- selector (paramètre)
-  // this.carrousel_id <- 'tekitizy_carroussel' ou options.carroussel_id
 }
 
 // Tekitizy.setup('.post img',{ carrousse_id: 'my-tekitizy' })
 // Tekitizy.setup('.post img')
 Tekitizy.setup = function (imgSelector, opts) {
   $(document).ready(function () {
-    tekitizy = new Tekitizy(imgSelector, opts)
+    var tekitizy = new Tekitizy(imgSelector, opts)
     tekitizy.setup()
   })
 }
@@ -30,32 +26,40 @@ Tekitizy.prototype.setup = function () {
   this.appendZoomBtn(this.selector, this.clickZoomBtn)
   this.listenToButtons()
   this.indexImages()
+  if (this.auto) {
+    this.actionPlay()
+  }
   // ...
 }
 
 Tekitizy.prototype.listenToButtons = function () {
-  // this -> instance Tekitizy
   var _this = this
   $('.tekitizy-open-btn').on('click', function () {
-    // this -> noeud
-    // _this -> instance Tekitizy
     _this.actionShow($(this))
+    if (_this.options.autoPlay) {
+      _this.actionPlay()
+    }
   })
 
   $('.tekitizy-close-btn').on('click', function () {
+    _this.actionPause()
     _this.actionClose()
   })
 
   $('.tekitizy-next-btn').on('click', function () {
+    _this.actionPause()
     _this.actionNext()
   })
 
   $('.tekitizy-prev-btn').on('click', function () {
+    _this.actionPause()
     _this.actionPrev()
   })
 
   $('.tekitizy-play-btn').on('click', function () {
-    _this.actionPlay()
+    if (!_this.auto) {
+      _this.actionPlay()
+    }
   })
 
   $('.tekitizy-pause-btn').on('click', function () {
@@ -69,13 +73,20 @@ Tekitizy.prototype.drawCarroussel = function (id) {
   carroussel += '<div class="tekitizy-carroussel" id=' + id + '>'
   carroussel += '<div class="tekitizy-image-container">'
   carroussel += '<img class="tekitizy-image-content" src="">'
+  if (this.options.effect) {
+    carroussel += '<img class="tekitizy-image-slide" src="">'
+  }
   carroussel += '<div class="tekitizy-image-title"></div>'
   carroussel += '</div>'
   carroussel += '<img class="tekitizy-button tekitizy-close-btn" src="../images/croix_rouge.png">'
-  carroussel += '<img class="tekitizy-button tekitizy-next-btn" src="../images/next.png">'
-  carroussel += '<img class="tekitizy-button tekitizy-prev-btn" src="../images/prev.png">'
-  carroussel += '<img class="tekitizy-button tekitizy-play-btn" src="../images/play.png">'
-  carroussel += '<img class="tekitizy-button tekitizy-pause-btn" src="../images/pause.png">'
+  if (this.options.prevNext) {
+    carroussel += '<img class="tekitizy-button tekitizy-next-btn" src="../images/next.png">'
+    carroussel += '<img class="tekitizy-button tekitizy-prev-btn" src="../images/prev.png">'
+  }
+  if (this.options.play) {
+    carroussel += '<img class="tekitizy-button tekitizy-play-btn" src="../images/play.png">'
+    carroussel += '<img class="tekitizy-button tekitizy-pause-btn" src="../images/pause.png">'
+  }
   carroussel += '</div>'
   // Ajouter les boutons, la figure ..
   this.carroussel = $(carroussel)
@@ -85,24 +96,25 @@ Tekitizy.prototype.drawCarroussel = function (id) {
 Tekitizy.prototype.appendZoomBtn = function (selector) {
   $(selector).each(function () {
     var $el
-    var image_src
+    var imageSrc
     $el = $(this)
-    image_src = $el.attr('src')
+    imageSrc = $el.attr('src')
     $el.wrap('<div></div>') // image
       .parent() // container
         .addClass('tekitizy-container') // container
-        .append('<i class="tekitizy-open-btn fa fa-search" data-src="' + image_src + '"  aria-hidden="true"></i>')
+        .append('<i class="tekitizy-open-btn fa fa-search" data-src="' + imageSrc + '"  aria-hidden="true"></i>')
   })
 }
 
 Tekitizy.prototype.indexImages = function () {
   var i = 0
   var _this = this
-  
   $('.tekitizy-container').each(function () {
-    $(this).find('.tekitizy-open-btn').attr('data-index', i)
+    var zoom = $(this).find('.tekitizy-open-btn')
+    zoom.attr('data-index', i)
     i++
-    _this.listImg.push($(this).find('img'))
+    var title = $(this).find('img').attr('alt')
+    _this.listImg.push({src: zoom.attr('data-src'), title: title  })
   })
 }
 
@@ -111,61 +123,83 @@ Tekitizy.prototype.actionShow = function (zoom) {
   this.carroussel.addClass('tekitizy-carroussel-open')
   var index = zoom.attr('data-index')
   this.index = index
-  var url = this.listImg[index].attr('src')
+  var url = this.listImg[index].src
   $('.tekitizy-image-content').attr('src', url)
-  console.log(this)
+  $('.tekitizy-image-title').html(this.listImg[index].title)
+  if (this.options.effect) {
+    var nextIndex = 0
+    if (this.listImg[parseInt(index) + 1]) {
+      nextIndex = parseInt(index) + 1
+    }
+    var urlSlide = this.listImg[nextIndex].src
+    $('.tekitizy-image-slide').attr('src', urlSlide)
+  }
 }
 
 Tekitizy.prototype.actionNext = function () {
-
   var index = parseInt(this.index) + 1
-  var nextimg = $('.tekitizy-open-btn[data-index="' + index + '"]')
   if (this.listImg[index]) {
-    var nextimgsrc = this.listImg[index].attr('src')
-    $('.tekitizy-image-content').attr('src', nextimgsrc)
+    if (this.options.effect) {
+      this.slideEffect()
+    } else {
+      var nextImgSrc = this.listImg[index].src
+      $('.tekitizy-image-content').attr('src', nextImgSrc)
+    }
+
+    $('.tekitizy-image-title').html(this.listImg[index].title)
     this.index = index
   }
 }
 
 Tekitizy.prototype.actionPrev = function () {
   var index = parseInt(this.index) - 1
-  var nextimg = $('.tekitizy-open-btn[data-index="' + index + '"]')
   if (this.listImg[index]) {
-    var nextimgsrc = this.listImg[index].attr('src')
-    $('.tekitizy-image-content').attr('src', nextimgsrc)
+    var prevImgSrc = this.listImg[index].src
+    $('.tekitizy-image-content').attr('src', prevImgSrc)
+    $('.tekitizy-image-title').html(this.listImg[index].title)
     this.index = index
   }
 }
 
 Tekitizy.prototype.actionPlay = function () {
+  this.actionPause()
   var _this = this
-  var direction = 'right'
-  this.auto = true
-  console.log(this)
-  var interval = setInterval(function() { 
-    if(_this.auto == false){
-      clearInterval(interval)
+  this.player = setInterval(function () {
+    if (_this.listImg.length - 1 == _this.index) {
+      _this.index = -1
     }
-    if(direction == 'right'){
-      _this.actionNext()
-      if(_this.listImg.length-1 == _this.index){
-        direction = 'left';
-      }
-    } else {
-      _this.actionPrev()
-      if(0 == _this.index){
-        direction = 'right';
-      }
-    }
-  }, _this.imageDuration*1000);
+    _this.actionNext()
+  }, _this.imageDuration * 1000)
 }
 
 Tekitizy.prototype.actionPause = function () {
-  this.auto = false;
+  clearInterval(this.player)
+  this.auto = false
 }
 
 Tekitizy.prototype.actionClose = function () {
   this.carroussel.removeClass('tekitizy-carroussel-open')
 }
 
-// Tekitizy.setup('.post img',{ 'carroussel_id': 'my-tekitizy-carroussel' })
+Tekitizy.prototype.slideEffect = function () {
+  var _this = this
+  var marginLeft = parseInt($('.tekitizy-image-content').css('marginLeft'))
+  var contentLeft = 2 * (marginLeft - $('.tekitizy-image-content').width())
+  $('.tekitizy-image-slide').css('display', 'inline-block')
+
+  $('.tekitizy-image-content').animate({
+    marginLeft: contentLeft,
+  }, 600, function () {
+    var contentSrc = $('.tekitizy-image-slide').attr('src')
+    $('.tekitizy-image-content').attr('src', contentSrc).css('marginLeft', marginLeft)
+
+    var index = 0
+    if (_this.listImg[_this.index + 1]) {
+      index = _this.index + 1
+    }
+
+    var slideSrc = _this.listImg[index].src
+
+    $('.tekitizy-image-slide').attr('src', slideSrc).css('display', 'none')
+  })
+}
