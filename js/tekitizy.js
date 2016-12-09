aef9f9a902f0d4c61da7fc1a63f249f0272725e7
Paul Girardin <paul.girardin@eemi.com>
2016-12-09 20:00:42 +0100

    Css and js debugged and title displays correctly

--- js/tekitizy.js
+++ js/tekitizy.js
@@ -20,6 +20,7 @@ Tekitizy.setup = function (imgSelector, opts) {
   $(document).ready(function () {
     var tekitizy = new Tekitizy(imgSelector, opts)
     tekitizy.setup()
+    tekitizy.actionPause()
   })
 }
 
@@ -28,7 +29,7 @@ Tekitizy.prototype.setup = function () {
   this.drawCarroussel(this.carroussel_id)
   this.appendZoomBtn(this.selector, this.clickZoomBtn)
   this.indexImages()
-  if(this.thumbnails){
+  if (this.thumbnails) {
     this.drawThumbNails()
   } // ...
   this.listenToButtons()
@@ -73,20 +74,19 @@ Tekitizy.prototype.listenToButtons = function () {
     _this.actionPause()
   })
 
-  if(this.thumbnails){
+  if (this.thumbnails) {
     $('.tekitizy-chevron-left').on('click', function () {
-      console.log('jepasseleft')
       _this.actionThumbPrev()
     })
 
     $('.tekitizy-chevron-right').on('click', function () {
       _this.actionThumbNext()
-    })   
+    })
 
     $('.tekitizy-thumbnail-image-content').on('click', function () {
       var index = $(this).attr('data-index')
       _this.actionShowThumb(index)
-    })  
+    })
   }
 }
 
@@ -108,42 +108,39 @@ Tekitizy.prototype.drawCarroussel = function (id) {
     carroussel += '<i class="fa fa-pause-circle-o tekitizy-button tekitizy-pause-btn" aria-hidden="true"></i>'
   }
   carroussel += '</div>'
-  carroussel += '<div class="tekitizy-image-title"></div>'
+  carroussel += '<div class="tekitizy-image-title tekitizy-carroussel-closed"></div>'
   carroussel += '</div>'
   carroussel += '</div>'
   this.carroussel = $(carroussel)
   this.carroussel.appendTo($('body'))
 }
 
-
-/* Add all images in tekitizy container*/
-Tekitizy.prototype.drawImages= function (id) {
-  _this = this
+/* Add all images in tekitizy container */
+Tekitizy.prototype.drawImages = function (id) {
+  var _this = this
   var margin = $('.tekitizy-image-slider').width()
-  $.each( _this.listImg, function( key, value ) {
-    $('.tekitizy-list-container').append('<div class="tekitizy-image-container"><img class="tekitizy-image-content" data-index="'+key+'" src="'+value.src+'"></div>').width()
-    $('.tekitizy-image-container').last().css('margin-left', margin*key)
-  });
+  $.each(_this.listImg, function (key, value) {
+    $('.tekitizy-list-container').append('<div class="tekitizy-image-container"><img class="tekitizy-image-content" data-index="' + key + '" src="' + value.src + '"></div>').width()
+    $('.tekitizy-image-container').last().css('margin-left', margin * key)
+  })
   $('.tekitizy-image-container').css('width', margin)
 }
 
 /* Construction of thumbnails */
-Tekitizy.prototype.drawThumbNails= function () {
-  _this = this
+Tekitizy.prototype.drawThumbNails = function () {
+  var _this = this
   $('.tekitizy-carroussel').append('<div class="tekitizy-thumbnail"><div class="thumbnail-list-container"></div></div>')
   $('.tekitizy-thumbnail').append('<div class="tekitizy-chevron-left"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>')
-  $.each( _this.listImg, function( key, value ) {
-    if( key%3 == 0){
-      _this.page = key/3
+  $.each(_this.listImg, function (key, value) {
+    if (key % 3 === 0) {
+      _this.page = key / 3
     }
-    $('.thumbnail-list-container').append('<div class="tekitizy-thumbnail-image-container"><img class="tekitizy-thumbnail-image-content" data-page="'+ _this.page +'" data-index="'+key+'" src="'+value.src+'"></div>')
-  });
+    $('.thumbnail-list-container').append('<div class="tekitizy-thumbnail-image-container"><img class="tekitizy-thumbnail-image-content" data-page="' + _this.page + '" data-index="' + key + '" src="' + value.src + '"></div>')
+  })
   var width = $('.tekitizy-thumbnail-image-container').width()
-  console.log(_this.listImg.length)
-  $('.thumbnail-list-container').css('width', width*(_this.listImg.length)+ 10*_this.listImg.length)
+  $('.thumbnail-list-container').css('width', width * (_this.listImg.length) + 10 * _this.listImg.length)
   _this.page = 0
   $('.tekitizy-thumbnail').append('<div class="tekitizy-chevron-right"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>')
-
 }
 
 /* Add a zoom in each image in element */
@@ -152,16 +149,14 @@ Tekitizy.prototype.appendZoomBtn = function (selector) {
     var $el
     var imageSrc
     $el = $(this)
-    var width = $el.width()
     imageSrc = $el.attr('src')
     $el.wrap('<div></div>') // image
       .parent() // container
         .addClass('tekitizy-container') // container
         .append('<i class="tekitizy-open-btn fa fa-search" data-src="' + imageSrc + '"  aria-hidden="true"></i>')
-    console.log($el.parent().css('width',width))
   })
 }
- 
+
 Tekitizy.prototype.indexImages = function () {
   var i = 0
   var _this = this
@@ -170,40 +165,41 @@ Tekitizy.prototype.indexImages = function () {
     zoom.attr('data-index', i)
     i++
     var title = $(this).find('img').attr('alt')
-    _this.listImg.push({src: zoom.attr('data-src'), title: title  })
+    _this.listImg.push({ src: zoom.attr('data-src'), title: title })
   })
 }
 
   // affiche une image
 Tekitizy.prototype.actionShow = function (zoom) {
   this.carroussel.addClass('tekitizy-carroussel-open')
+  this.carroussel.removeClass('tekitizy-carroussel-closed')
   var index = zoom.attr('data-index')
   this.index = index
   var margin = $('.tekitizy-image-slider').width()
-  $('.tekitizy-list-container').css('margin-left', '-'+margin*index+'px')
+  $('.tekitizy-image-title').html(this.listImg[index].title)
+  $('.tekitizy-list-container').css('margin-left', '-' + margin * index + 'px')
 }
 
 Tekitizy.prototype.actionNext = function () {
-  _this = this
+  var _this = this
   var index = parseInt(this.index) + 1
-  var margin =  $('.tekitizy-image-slider').width()
   if (this.listImg[index]) {
-    if(this.thumbnails){
-      $('.tekitizy-thumbnail-image-content[data-index="'+this.index+'"]').css('opacity','0.5')
+    if (this.thumbnails) {
+      $('.tekitizy-thumbnail-image-content[data-index="' + this.index + '"]').css('opacity', '0.5')
     }
-    if(this.effect){
-      $( ".tekitizy-list-container" ).animate({
-          marginLeft: '-'+index*100+'%'
-      }, 500);    
+    if (this.effect) {
+      $('.tekitizy-list-container').animate({
+        marginLeft: '-' + index * 100 + '%'
+      }, 500)
     } else {
-      $( ".tekitizy-list-container" ).css('margin-left' , '-'+index*100+'%')        
+      $('.tekitizy-list-container').css('margin-left', '-' + index * 100 + '%')
     }
     $('.tekitizy-image-title').html(this.listImg[index].title)
     this.index = index
-    if(this.thumbnails){
-      $('.tekitizy-thumbnail-image-content[data-index="'+index+'"]').css('opacity','1')
-      if(index%3 == 0){
-      _this.actionThumbPage()      
+    if (this.thumbnails) {
+      $('.tekitizy-thumbnail-image-content[data-index="' + index + '"]').css('opacity', '1')
+      if (index % 3 === 0) {
+        _this.actionThumbPage()
       }
     }
   }
@@ -211,25 +207,25 @@ Tekitizy.prototype.actionNext = function () {
 
 Tekitizy.prototype.actionPrev = function () {
   var index = parseInt(this.index) - 1
-  var margin = $('.tekitizy-image-slider').width()
+  var _this = this
   if (this.listImg[index]) {
-    if(this.thumbnails){
-      $('.tekitizy-thumbnail-image-content[data-index="'+this.index+'"]').css('opacity','0.5')
+    if (this.thumbnails) {
+      $('.tekitizy-thumbnail-image-content[data-index="' + this.index + '"]').css('opacity', '0.5')
     }
-    if(this.effect){
-      $( ".tekitizy-list-container" ).animate({
-          marginLeft: '-'+index*100+'%'
-      }, 500);    
+    if (this.effect) {
+      $('.tekitizy-list-container').animate({
+        marginLeft: '-' + index * 100 + '%'
+      }, 500)
     } else {
-      $( ".tekitizy-list-container" ).css('margin-left' , '-'+index*100+'%')        
+      $('.tekitizy-list-container').css('margin-left', '-' + index * 100 + '%')
     }
 
     $('.tekitizy-image-title').html(this.listImg[index].title)
     this.index = index
-    if(this.thumbnails){
-      $('.tekitizy-thumbnail-image-content[data-index="'+index+'"]').css('opacity','1')
-      if(index%3 == 2){
-      _this.actionThumbPage()      
+    if (this.thumbnails) {
+      $('.tekitizy-thumbnail-image-content[data-index="' + index + '"]').css('opacity', '1')
+      if (index % 3 === 2) {
+        _this.actionThumbPage()
       }
     }
   }
@@ -239,7 +235,7 @@ Tekitizy.prototype.actionPlay = function () {
   this.actionPause()
   var _this = this
   this.player = setInterval(function () {
-    if (_this.listImg.length - 1 == _this.index) {
+    if (_this.listImg.length - 1 === parseInt(_this.index)) {
       _this.index = -1
     }
     _this.actionNext()
@@ -247,64 +243,58 @@ Tekitizy.prototype.actionPlay = function () {
 }
 
 Tekitizy.prototype.actionPause = function () {
-  this.auto = false;
+  this.auto = false
   clearInterval(this.player)
 }
 
 Tekitizy.prototype.actionClose = function () {
   this.actionPause()
   this.carroussel.removeClass('tekitizy-carroussel-open')
+  this.carroussel.addClass('tekitizy-carroussel-closed')
 }
 
 Tekitizy.prototype.actionShowThumb = function (index) {
   this.actionPause()
-  $('.tekitizy-thumbnail-image-content[data-index="'+this.index+'"]').css('opacity','0.5')
+  $('.tekitizy-thumbnail-image-content[data-index="' + this.index + '"]').css('opacity', '0.5')
   this.index = index
-  var margin = $('.tekitizy-image-slider').width()
-  if(this.effect){
-    $( ".tekitizy-list-container" ).animate({
-        marginLeft: '-'+index*100+'%'
-    }, 500);    
+  if (this.effect) {
+    $('.tekitizy-list-container').animate({
+      marginLeft: '-' + index * 100 + '%'
+    }, 500)
   } else {
-    $( ".tekitizy-list-container" ).css('margin-left' , '-'+index*100+'%')        
+    $('.tekitizy-list-container').css('margin-left', '-' + index * 100 + '%')
   }
-  $('.tekitizy-thumbnail-image-content[data-index="'+this.index+'"]').css('opacity','1')
+  $('.tekitizy-thumbnail-image-content[data-index="' + this.index + '"]').css('opacity', '1')
 }
 
 Tekitizy.prototype.actionThumbPage = function () {
-  var margin = $('.tekitizy-thumbnail').width()
-  _this = this
-  var currentPage= $('.tekitizy-thumbnail-image-content[data-index="'+_this.index+'"]').attr('data-page')
+  var _this = this
+  var currentPage = $('.tekitizy-thumbnail-image-content[data-index="' + _this.index + '"]').attr('data-page')
   _this.page = currentPage
-  $( ".thumbnail-list-container" ).animate({
-      marginLeft: '-'+currentPage*100+'%'
-  }, 500);
+  $('.thumbnail-list-container').animate({
+    marginLeft: '-' + currentPage * 100 + '%'
+  }, 500)
 }
 
 Tekitizy.prototype.actionThumbNext = function () {
-  var maxPage = Math.ceil(this.listImg.length/3)
+  var maxPage = Math.ceil(this.listImg.length / 3)
   this.actionPause()
-  var margin = $('.tekitizy-thumbnail').width()
-  _this = this
-  if(this.page+1 < maxPage){
-    _this.page = _this.page + 1
-    $( ".thumbnail-list-container" ).animate({
-        marginLeft: '-'+_this.page*100+'%'
-    }, 500);   
+  if (parseInt(this.page) + 1 < maxPage) {
+    this.page = parseInt(this.page) + 1
   }
+  var _this = this
+  $('.thumbnail-list-container').animate({
+    marginLeft: '-' + _this.page * 100 + '%'
+  }, 500)
 }
 
 Tekitizy.prototype.actionThumbPrev = function () {
   this.actionPause()
-  console.log('je passe')
-  var maxPage = Math.ceil(this.listImg.length/3)
-  var margin = $('.tekitizy-thumbnail').width()
-  _this = this
-  if(this.page-1 >= 0){
-    _this.page = _this.page-1
-    $( ".thumbnail-list-container" ).animate({
-        marginLeft: '-'+_this.page*100+'%'
-    }, 500);
+  if (parseInt(this.page) - 1 >= 0) {
+    this.page = parseInt(this.page) - 1
   }
-
+  var _this = this
+  $('.thumbnail-list-container').animate({
+    marginLeft: '-' + _this.page * 100 + '%'
+  }, 500)
 }
